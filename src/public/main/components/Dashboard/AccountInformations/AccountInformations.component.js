43d6ee93ed2c21eabb16420acde62b267b722c4e
Léo RASPAUD <leo.raspaud@gmail.com>
2018-01-21 19:44:23 +1100

    Add loading on total

--- src/public/main/components/Dashboard/AccountInformations/AccountInformations.component.js
+++ src/public/main/components/Dashboard/AccountInformations/AccountInformations.component.js
@@ -11,6 +11,22 @@ const THRESHOLD = 0.001;
 
 const filterLowCountItems = balance => (balance.free || balance.locked) > THRESHOLD;
 
+const getCssTrends = ({ previousPrice, lastPrice }) => {
+    let cssClass;
+    let icon;
+    if (lastPrice > previousPrice) {
+        cssClass = 'trendingUp';
+        icon = 'trending_up';
+    } else if (lastPrice < previousPrice) {
+        cssClass = 'trendingDown';
+        icon = 'trending_down';
+    } else {
+        cssClass = 'trendingFlat';
+        icon = 'trending_flat';
+    }
+    return { icon, cssClass };
+};
+
 @withStyles(styles)
 class AccountInformations extends React.Component {
     static propTypes = {
@@ -23,6 +39,9 @@ class AccountInformations extends React.Component {
 
     state = {
         displayedItems: this.props.data.data.balances.filter(filterLowCountItems).map(item => ({ balance: item, loading: true })),
+        total: 0,
+        previousTotal: 0,
+        itemsToLoad: this.props.data.data.balances.filter(filterLowCountItems).map(item => ({ balance: item, loading: true })).length,
     }
 
     componentDidMount = async () => {
@@ -39,18 +58,7 @@ class AccountInformations extends React.Component {
         const { prices } = trend;
         const previousPrice = prices[0].value;
         const lastPrice = prices[1].value;
-        let cssClass;
-        let icon;
-        if (lastPrice > previousPrice) {
-            cssClass = 'trendingUp';
-            icon = 'trending_up';
-        } else if (lastPrice < previousPrice) {
-            cssClass = 'trendingDown';
-            icon = 'trending_down';
-        } else {
-            cssClass = 'trendingFlat';
-            icon = 'trending_flat';
-        }
+        const { cssClass, icon } = getCssTrends({ previousPrice, lastPrice });
         element.cssClass = cssClass;
         element.icon = icon;
         element.loading = false;
@@ -58,12 +66,21 @@ class AccountInformations extends React.Component {
         if (element.balance.asset !== 'ETH') {
             element.lastPrice *= lastPrice;
         }
-        element.lastPrice = element.lastPrice.toFixed(2);
-        this.setState({ displayedItems: this.state.displayedItems });
+        element.lastPrice = Number.parseFloat(element.lastPrice.toFixed(2));
+        this.setState({
+            displayedItems: this.state.displayedItems,
+            total: this.state.total + element.lastPrice,
+            itemsToLoad: this.state.itemsToLoad - 1,
+        });
     };
 
     refreshValues = async () => {
-        this.setState({ displayedItems: this.state.displayedItems.map(item => ({ ...item, loading: true })) });
+        this.setState({
+            displayedItems: this.state.displayedItems.map(item => ({ ...item, loading: true })),
+            previousTotal: this.state.total,
+            total: 0,
+            itemsToLoad: this.state.displayedItems.length,
+        });
         const { getTrend, data, getLastPrice } = this.props;
         const resultLastPrice = await getLastPrice({ exchangeName: data.exchangeName, symbol: 'ETHUSDT' });
         const lastEthPrice = resultLastPrice.prices;
@@ -82,9 +99,25 @@ class AccountInformations extends React.Component {
     /* eslint-disable jsx-a11y/click-events-have-key-events */
     render() {
         const { classes, data, displayPrices } = this.props;
+        const { cssClass, icon } = getCssTrends({ previousPrice: this.state.previousTotal, lastPrice: this.state.total });
         return (
             <Paper className={classes.paperWrapper}>
-                <div className={classes.title}>{data.exchangeName}</div>
+                <div className={classes.title}>
+                    <div style={{ flex: 1 }}>
+                        {data.exchangeName}
+                    </div>
+                    {this.state.itemsToLoad !== 0 && <CircularProgress color="primary" size={24} style={{ marginRight: 15 }} />}
+                    {this.state.itemsToLoad === 0 && (
+                        <div style={{ display: 'flex', alignItems: 'center', marginRight: 10 }} className={cssClass}>
+                            <Icon style={{ marginRight: 5 }} className={classes[cssClass]}>
+                                {icon}
+                            </Icon>
+                            <div>
+                                ${this.state.total.toFixed(2)}
+                            </div>
+                        </div>
+                    )}
+                </div>
                 <div className={classes.assetWrapper} >
                     {this.state.displayedItems.map(item => (
                         <div
@@ -118,7 +151,7 @@ class AccountInformations extends React.Component {
                                 {item.loading && <CircularProgress color="primary" size={24} />}
                                 {!item.loading && (
                                     <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <Icon style={{ marginRight: 5, color: item.color }} className={classes[item.cssClass]}>
+                                        <Icon style={{ marginRight: 5 }} className={classes[item.cssClass]}>
                                             {item.icon}
                                         </Icon>
                                         <div>
